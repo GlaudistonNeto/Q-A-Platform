bded3d9b743fa2cc17478df74bf10cbdf01e3ccf
Glaudiston Santos de Oliveira Neto <39042130+GlaudistonNeto@users.noreply.github.com>
2020-01-24 23:33:46 -0300

    changing host

--- database/database.js
+++ database/database.js
@@ -1,8 +1,9 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const connection = new Sequelize('guiaperguntas', 'root', 'spkdev', {
-    host: 'localhost',
-    dialect: 'mysql'
+const connection = new Sequelize('gson-press', 'gson', 'spk32Dev', {
+    host: 'mysql669.umbler.com',
+    dialect: 'mysql',
+    timezone: '-03:00'
 });
 
 module.exports = connection;
