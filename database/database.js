e9799111cad315528790214cbd30211276a03e06
Glaudiston Santos de Oliveira Neto <39042130+GlaudistonNeto@users.noreply.github.com>
2020-01-24 23:54:20 -0300

    correcting database

--- database/database.js
+++ database/database.js
@@ -1,9 +1,8 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
-const connection = new Sequelize('gson-press', 'gson', 'spk32Dev', {
-    host: 'mysql669.umbler.com',
-    dialect: 'mysql',
-    timezone: '-03:00'
+const connection = new Sequelize('guiaperguntas', 'root', 'spkdev', {
+    host: 'localhost',
+    dialect: 'mysql'
 });
 
 module.exports = connection;
